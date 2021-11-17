b88dfb8e56af35fc2eac1788f49b2e8b34c7d341
SakshamAnzo <90999858+SakshamAnzo@users.noreply.github.com>
2021-11-17 14:33:48 +0530

    add1

--- hello.cpp
+++ hello.cpp
@@ -8,7 +8,7 @@ return 0;
       printf("hello hi every one\n");
       printf(" hi every \n");
 
-      printf("hello hi every \n")
+      printf("hello hi every \n");
 	printf("test pre-push \n");
 
       return -1;
