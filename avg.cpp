fe195b60ae03fb913aeadc3c371a5654c6c45578
SakshamAnzo <90999858+SakshamAnzo@users.noreply.github.com>
2021-11-18 14:54:09 +0530

    g

--- avg.cpp
+++ avg.cpp
@@ -6,8 +6,13 @@ int main()
 
     double average = 0;
     average = 64;
+    cout << "enter number\n";
+    cin >> average >> endl;
 
     cout << "\n\nThe Average of the  is : " << average << endl;
-    return 0;
+    if (average < 90 && average > 50)
+     return 0;
+    else 
+	    return -1;
 }
 
--- run-tests.sh
+++ run-tests.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 make test1
 make run
+# if [ $? -ne 0 ]; then
+#  echo "Tests must pass before commit!"
+#  exit 1
+# fi
+#  exit 0
