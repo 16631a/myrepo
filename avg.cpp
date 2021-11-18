f2cd4a44753f272f0547ed7484c6c2599822ee6f
SakshamAnzo <90999858+SakshamAnzo@users.noreply.github.com>
2021-11-18 12:51:18 +0530

    1

--- avg.cpp
+++ avg.cpp
@@ -1,13 +1,30 @@
 #include <iostream>
+using namespace std;
 
 int main()
 {
-      int avg=0,value=0,i=0;
-      std::cout<<"enter 5 no one by one:\n";
-      for(i=0;i<6;i++)
-      {
-       std::cin >> value;
-       avg +=value;
-      }
-      return avg;
+
+    int n, i, temp;
+
+    double sum = 0;
+
+    double average = 0;
+
+    cout << " Enter the number of integers you want to find the average of : ";
+    cin >> n;
+    cout << "\n\n";
+
+    for (i = 0; i < n; i++)
+    {
+        cout << "Enter number" << i + 1 << " :  ";
+        cin >> temp;
+
+        sum += temp;
+    }
+
+    average = sum / n;
+
+    cout << "\n\nThe Average of the " << n << " numbers entered by the user is : " << average << endl;
+    return 0;
 }
+
