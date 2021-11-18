4029c08f6daa955e3f82a0fc27af6b74e74826b3
SakshamAnzo <90999858+SakshamAnzo@users.noreply.github.com>
2021-11-18 14:45:04 +0530

    l

--- avg.cpp
+++ avg.cpp
@@ -4,26 +4,10 @@ using namespace std;
 int main()
 {
 
-    int n, i, temp;
-
-    double sum = 0;
     double average = 0;
-
-    cout << " Enter the number of integers you want to find the average of : ";
-    // cin >> n;
-    cout << "\n\n";
-
-    for (i = 0; i < n; i++)
-    {
-        cout << "Enter number" << i + 1 << " :  ";
-       // cin >> temp;
-        sum += temp;
-    }
-
-    average = sum / n;
     average = 64;
 
-    cout << "\n\nThe Average of the " << n << " numbers entered by the user is : " << average << endl;
-    return 64;
+    cout << "\n\nThe Average of the  is : " << average << endl;
+    return 0;
 }
 
---
+++ run-tests.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+make test1
+make run
